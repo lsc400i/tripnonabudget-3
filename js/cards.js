a50f74d4f632cac51e94cde5c30a90c5d929edbc
lsc400i <chuong.tu.huynh@gmail.com>
2015-08-08 10:05:37 -0700

    Changes to account.html, card.css, main.css, added icons
    Took out extra templates from account.html, simplify code. Added
    back_button icon and plus-icon to interface. Updated bucket_list.css,
    added styles for items.

--- js/cards.js
+++ js/cards.js
@@ -1,65 +1,64 @@
+// An object that will eventually let us look up movies by a unique ID.
 var tripsDictionary = {};
 
 function fetchData() {
   var rawTemplate = $('#tripList-template').html();
-    $.get('https://trip-n-on-a-budget.firebaseio.com/trips.json', function(trips) {
+
+  $.get('https://trip-n-on-a-budget.firebaseio.com/trips.json', function(trips) {
+    // Stamp out cards and append them into <div id="cards-container"></div>
     for (var i = 0; i < trips.length; i++) {
       var currentTrip = trips[i];
       var stampedTemplate = Mustache.render(rawTemplate, currentTrip);
-  $('#tripsList-container').append(stampedTemplate);
-  
-  };
+      $('#tripsList-container').append(stampedTemplate);
+    };
 
-// function fetchData() {
-//   var rawTemplate = $('#bucketList-template').html();
-//     $.get('https://trip-n-on-a-budget.firebaseio.com/trips.json', function(trips) {
-//     for (var i = 0; i < trips.length; i++) {
-//       var currentTrip = trips[i];
-//       var stampedTemplate = Mustache.render(rawTemplate, currentTrip);
-//   $('#bucketList-container').append(stampedTemplate);
-  
-//   };
-// buildDictionary(trips);
-bindEventListeners();
-
-});
+    // Our "pre-processing" step where we take the movies array we got from
+    // our database and creates an object out of it.
+    buildDictionary(trips);
+    bindEventListeners();
+  });
 
 }
 
-
-function bindEventListeners () {
+function bindEventListeners() {
+  // Whenever any card gets clicked, show the lightbox.
   $('.card').click(function(e) {
-    var targetID = e.target.id;
-    var info = tripsDictionary[targetID];
+    // e.target is the <div> that got clicked. The ID of that div holds a key
+    // we can use to "look up" movie info in our moviesDictionary. Tricky.
+    var targetId = e.target.id;
+    var info = tripsDictionary[targetId];
 
-    var rawTemplate = $('#bucketList-container').html();
+    if (Array.isArray(info.bucketList)) {
+      info.bucketList = info.bucketList.join(', ');
+    }
+    
+    if (Array.isArray(info.checkList)) {
+      info.checkList = info.checkList.join(', ');
+    }
+
+    // Now that we have our info, simply stamp out our lightbox template.
+    var rawTemplate = $('#tripsDetails-template').html();
     var stampedTemplate = Mustache.render(rawTemplate, info);
-    $('#bucketList-container').html(stampedTemplate);
+    $('#tripsDetails-container').html(stampedTemplate);
+    $('#tripsDetails-container').animate({left: '38%'}, 750);
     
-// Transition Effect
-bucketList-container.css('transform', 'translateY(0)');
 
+  });
+
+  // Fade out the lightbox and dimmer mask when the mask is clicked.
+  $('#tripsDetails-container').click(function() {
+    $('#tripsDetails-container').animate({left: '-100%'}, 750);
     
-    
-});
-
-$('#mask').click(function() {
-  $('#tripsList-container').fadeOut();
-  $('#mask').fadeOut();
-
- });
-
+  });
 }
 
-function buildDictionary(trips){
-for (var i = 0; i < trips.length; i++) {
-  var currentTrip = trips[i];
-  tripsDictionary[currentTrips.id] = currentTrips;
-
-
-};
+function buildDictionary(trips) {
+  // Take the movies array and convert it into an object.
+  // moviesDictionary holds that object and we can use it to do lookups.
+  for (var i = 0; i < trips.length; i++) {
+    var currentTrip = trips[i];
+    tripsDictionary[currentTrip.id] = currentTrip;
+  }
 }
-
-
 
 fetchData();
